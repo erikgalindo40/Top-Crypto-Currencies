be8a1a9693dfa021e5dbd2d83c002bd318c04c7c
Diego Figueroa <diegofigueroa79@gmail.com>
2022-11-01 13:40:45 -0700

    add CryptoContainer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import CryptoContainer from "./components/CryptoContainer";
 
 function App() {
 
   return (
     <>
     <Header />
+    <CryptoContainer />
     <Footer />
     </>
   );
---
+++ src/components/CryptoContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CryptoContainer() {
+  return (
+    <div>
+        <CryptoHeader />
+    </div>
+  )
+}
+// example
+//{crypto.map( crypto => (
+//    <Cryptocurrency price={crypto.price} />
+//))}
+
+// rank, name, price, market cap, volume and circulating supply
+
+export default CryptoContainer
