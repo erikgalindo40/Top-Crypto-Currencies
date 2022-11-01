3c50d6032b22d957dd4cf6dcc833a70dcfdb2ab2
Diego Figueroa <diegofigueroa79@gmail.com>
2022-11-01 13:22:08 -0700

    add Header components and styling

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Headersearch from "./Headersearch"
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <h1>Top Crypto Currencies</h1>
+            <Headersearch />
+        </div>
+    )
+}
---
+++ src/components/Headersearch.js
@@ -0,0 +1,10 @@
+
+const Headersearch = props => {
+    return (
+        <form>
+            <input type="text" placeholder="Search" />
+        </form>
+    )
+}
+
+export default Headersearch
