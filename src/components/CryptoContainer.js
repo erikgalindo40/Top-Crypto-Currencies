03deb712b2a00cbb535f4fe6712ee636242a0e6f
Diego Figueroa <diegofigueroa79@gmail.com>
2022-11-02 12:39:22 -0700

    add mapping function to Cryptocurrency react element

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import CryptoHeader from './CryptoHeader'
 import Cryptocurrency from './Cryptocurrency'
 
 function CryptoContainer() {
+  const { data } = useSelector( (state) => state.currency )
+  
   return (
     <div>
         <CryptoHeader />
-        <Cryptocurrency/>
+        {
+          data.map( cryptocurrency => (
+            <Cryptocurrency
+              rank={data.id}
+              name={data.name}
+              price={data.market_pairs[0].quote.USD.price}
+              marketCap={100000000}
+              volume={data.market_pairs[0].quote.USD.volume_24h}
+              circulatingSupply={21000000}
+            />
+          ))
+        }
     </div>
   )
 }
