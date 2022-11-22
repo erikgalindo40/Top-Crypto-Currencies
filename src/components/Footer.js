1f40e4aec901411fc16b326187c1cc8e27042818
erikgalindo40 <erikgalindo40@yahoo.com>
2022-11-21 19:15:08 -0800

    remove footer fine print and remove company logo from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,7 +48,7 @@ function Footer() {
   return (
     <footer className='footer-container'>
         <div className='footer-title'>
-            <img src="https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_1.svg?_=872fb24" alt="logo" />
+            Top Currencies
         </div>
         <div className='footer-lists'>
             <Footerlist title={productFooterObject.title} links={productFooterObject.productList}/>
@@ -56,9 +56,6 @@ function Footer() {
             <Footerlist title={supportFooterObject.title} links={supportFooterObject.productList}/>
             <Footerlist title={socialsFooterObject.title} links={socialsFooterObject.productList}/>
         </div>
-        {/* <div className='copyright-info'>
-            &copy; 2022 CoinMarketCap. All Rights Reserved
-        </div> */}
     </footer>
   )
 }
