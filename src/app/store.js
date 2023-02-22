aa0f46f633a9cbdac9bda2d81cab2d345f1636b1
Erik Galindo <erikgalindo40@yahoo.com>
2023-02-22 13:49:24 -0800

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import currencyReducer from '../features/currency/currencySlice';
-
-export const store = configureStore({
-  reducer: {
-    currency: currencyReducer,
-  },
-});
